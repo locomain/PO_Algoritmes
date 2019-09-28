0de65295293eb857caa418a483dc6b05190cd6f4
Angelo <locomain12@gmail.com>
2019-09-28 15:47:14 +0200

    fixed the sort

--- src/quicksort/quicksorter.dart
+++ src/quicksort/quicksorter.dart
@@ -6,27 +6,27 @@ class Quicksorter{
   static List sort<T extends Comparable>(List<T> list){
     if (list.length <= 1) return list;
 
-    final T pivot = Quicksorter.getMedian(list);
+    final int pivotIndex = Quicksorter.getMedian(list);
+    final T pivot = list[pivotIndex];
     final List<T> left = List<T>();
     final List<T> right = List<T>();
 
-    for(T member in list){
+    for(int i = 0; i< list.length; i++){
+      if(i==pivotIndex)continue;
 
+      final T member = list[i];
       if(member.compareTo(pivot)<=0)left.add(member);
       if(member.compareTo(pivot)>0)right.add(member);
     }
 
-    return [...Quicksorter.sort(left),...Quicksorter.sort(right)];
+    return [...Quicksorter.sort(left), pivot, ...Quicksorter.sort(right)];
   }
 
   /**
    * Gets the median of the list
    */
-  static T getMedian<T extends Comparable>(List<T>list ){
-    T center = list[list.length~/2];
-    T middle = list[list.length~/2];
-    T last = list[list.length~/2];
-    return last;
+  static int getMedian<T>(List<T> list){
+    return list.length~/2;
   }
 
 }
